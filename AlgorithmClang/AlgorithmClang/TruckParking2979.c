7f0741e5f5036ad0226d2ff13a4e833784513390
sexyseunghwan <67136763+sexyseunghwan@users.noreply.github.com>
2020-06-20 01:14:55 +0900

    test commit

---
+++ AlgorithmClang/AlgorithmClang/TruckParking2979.c
@@ -0,0 +1,78 @@
+﻿
+#include <stdio.h>
+
+
+/*
+상근이는 트럭을 총 세 대 가지고 있다. 오늘은 트럭을 주차하는데 비용이 얼마나 필요한지 알아보려고 한다.
+
+상근이가 이용하는 주차장은 주차하는 트럭의 수에 따라서 주차 요금을 할인해 준다.
+
+트럭을 한 대 주차할 때는 1분에 한 대당 A원을 내야 한다. 두 대를 주차할 때는 1분에 한 대당 B원, 세 대를 주차할 때는 1분에 한 대당 C원을 내야 한다.
+
+A, B, C가 주어지고, 상근이의 트럭이 주차장에 주차된 시간이 주어졌을 때, 주차 요금으로 얼마를 내야 하는지 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 문제에서 설명한 주차 요금 A, B, C가 주어진다. (1 ≤ C ≤ B ≤ A ≤ 100)
+
+다음 세 개 줄에는 두 정수가 주어진다. 이 정수는 상근이가 가지고 있는 트럭이 주차장에 도착한 시간과 주차장에서 떠난 시간이다. 도착한 시간은 항상 떠난 시간보다 앞선다. 입력으로 주어지는 시간은 1과 100사이 이다.
+
+출력
+첫째 줄에 상근이가 내야하는 주차 요금을 출력한다.
+
+
+예제 입력 1
+5 3 1
+1 6
+3 5
+2 8
+
+
+예제 출력 1
+33
+
+*/
+
+
+
+int table[3][101];//내용을 모두 0 으로 초기화 시켜준다.
+
+int main_2979()
+{
+	int one_fee, two_fee, three_fee;
+
+	int total_fee = 0;
+
+	scanf("%d %d %d", &one_fee, &two_fee, &three_fee);
+
+
+	for (int i = 0; i < 3; i++) {
+		int in_time, out_time;
+
+		scanf("%d %d", &in_time, &out_time);
+
+		for (int j = in_time; j < out_time; j++) {
+			table[i][j] = 1;
+		}
+	}
+
+	for (int i = 1; i < 101; i++) {
+		int count = table[0][i] + table[1][i] + table[2][i];
+
+		switch (count) {
+		case 1:
+			total_fee += one_fee;
+			break;
+		case 2:
+			total_fee += two_fee * 2;
+			break;
+		case 3:
+			total_fee += three_fee * 3;
+			break;
+		}
+	}
+
+	printf("%d", total_fee);
+
+
+	return 0;
+}
